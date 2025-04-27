a8e7ab7cb32b100a24eff5285709a32f077c6ab0
Javier Larre <javier.larre@uc.cl>
2025-04-27 19:46:45 -0400

    animacion de edificios añadida

--- ui.gd
+++ ui.gd
@@ -1,18 +1,32 @@
 extends CanvasLayer
 
-@onready var vbox = $ColorRect/Puertas
+@onready var vbox = $Puertas
 @onready var mouse_text = $MouseText
 
 func _ready() -> void:
+	mouse_text.z_index += 1
+	create_buttons()
+	play_hotel_animation()
+	$DoneButton.pressed.connect(play_leave_hotel_animation)
+	
+func create_buttons() -> void:
 	for i in range(3):
 		var button = create_button()
 		vbox.add_child(button)
-	
-func button_pressed() -> void:
-	mouse_text.visible = not mouse_text.visible
 
 func create_button() -> Button:
 	var button = Button.new()
 	button.text = "omg hazme click"
 	button.pressed.connect(button_pressed)
 	return button
+	
+func button_pressed() -> void:
+	mouse_text.visible = not mouse_text.visible
+
+func play_hotel_animation() -> void:
+	var hotelAnimation = $BuildingsView/Hotel/BuildingAnimation
+	hotelAnimation.play("Arriving")
+	
+func play_leave_hotel_animation() -> void:
+	var hotelAnimation = $BuildingsView/Hotel/BuildingAnimation
+	hotelAnimation.play("Leaving")
